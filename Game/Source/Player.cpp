616e0f72d814d75d92da3f8c11b5fcc3ebc702af
DavidLastra8 <david.lastra@estudiantat.upc.edu>
2023-10-28 12:05:48 +0200

    development 1.0

---
+++ Game/Source/Player.cpp
@@ -0,0 +1,108 @@
+#include "Player.h"
+#include "App.h"
+#include "Textures.h"
+#include "Audio.h"
+#include "Input.h"
+#include "Render.h"
+#include "Scene.h"
+#include "Log.h"
+#include "Point.h"
+#include "Physics.h"
+
+Player::Player() : Entity(EntityType::PLAYER)
+{
+	name.Create("Player");
+}
+
+Player::~Player() {
+
+}
+
+bool Player::Awake() {
+
+	position.x = parameters.attribute("x").as_int();
+	position.y = parameters.attribute("y").as_int();
+	texturePath = parameters.attribute("texturepath").as_string();
+
+	return true;
+}
+
+bool Player::Start() {
+
+	//initilize textures
+	texture = app->tex->Load(texturePath);
+
+	pbody = app->physics->CreateCircle(position.x + 16, position.y + 16, 16, bodyType::DYNAMIC);
+	pbody->listener = this;
+	pbody->ctype = ColliderType::PLAYER;
+
+	pickCoinFxId = app->audio->LoadFx("Assets/Audio/Fx/retro-video-game-coin-pickup-38299.ogg");
+
+	return true;
+}
+
+bool Player::Update(float dt)
+{
+	
+	vely = -GRAVITY_Y;
+	velx = 0;
+
+	if (app->input->GetKey(SDL_SCANCODE_W) == KEY_REPEAT) {
+		//
+	}
+	if (app->input->GetKey(SDL_SCANCODE_S) == KEY_REPEAT) {
+		//
+	}
+
+	if (app->input->GetKey(SDL_SCANCODE_A) == KEY_REPEAT) {
+		velx = -speed * dt;
+	}
+
+	if (app->input->GetKey(SDL_SCANCODE_D) == KEY_REPEAT) {
+		velx = speed * dt;
+	}
+
+	if (app->input->GetKey(SDL_SCANCODE_SPACE) == KEY_REPEAT) {
+		float jmptime=SDL_GetTicks();
+		
+		vely = GRAVITY_Y;
+	}
+	b2Vec2 vel = b2Vec2(velx, vely);
+	//Set the velocity of the pbody of the player
+	pbody->body->SetLinearVelocity(vel);
+
+	//Update player position in pixels
+	position.x = METERS_TO_PIXELS(pbody->body->GetTransform().p.x) - 16;
+	position.y = METERS_TO_PIXELS(pbody->body->GetTransform().p.y) - 16;
+
+	app->render->DrawTexture(texture, position.x, position.y);
+
+	return true;
+}
+
+bool Player::CleanUp()
+{
+
+	return true;
+}
+
+void Player::OnCollision(PhysBody* physA, PhysBody* physB) {
+
+	switch (physB->ctype)
+	{
+	case ColliderType::ITEM:
+		LOG("Collision ITEM");
+		app->audio->PlayFx(pickCoinFxId);
+		break;
+	case ColliderType::PLATFORM:
+		LOG("Collision PLATFORM");
+		if (Isjumping) {
+			Isjumping = false;
+		}
+		break;
+
+	case ColliderType::UNKNOWN:
+		LOG("Collision UNKNOWN");
+		break;
+	}
+}
