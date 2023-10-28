8f6876f7c8b461cc736c0f1c0c92c7007bb24456
Yeeway69 <yeyiwei1@gmail.com>
2023-10-28 13:24:36 +0200

    v1.0.1
    Following the assignment guideline document, now it's press f9 to Visualize the colliders / logic

--- Game/Source/Physics.cpp
+++ Game/Source/Physics.cpp
@@ -224,7 +224,7 @@ bool Physics::PostUpdate()
 	bool ret = true;
 
 	// Activate or deactivate debug mode
-	if (app->input->GetKey(SDL_SCANCODE_F1) == KEY_DOWN)
+	if (app->input->GetKey(SDL_SCANCODE_F9) == KEY_DOWN)
 		debug = !debug;
 	
 	//  Iterate all objects in the world and draw the bodies
