16e1d1ae1d7b104ed6fe06c76f4761f55fbb3683
DavidLastra8 <david.lastra@estudiantat.upc.edu>
2023-10-31 21:18:29 +0100

    tsx map

---
+++ Output/Assets/Maps/Mossy - Decorations&Hazards.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Mossy - Decorations&amp;Hazards" tilewidth="32" tileheight="32" tilecount="16384" columns="128">
+ <image source="Downloads/sprites tiled/Mossy - Decorations&amp;Hazards.png" width="4096" height="4096"/>
+</tileset>
---
+++ Output/Assets/Maps/Mossy - TileSet.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Mossy - TileSet" tilewidth="32" tileheight="32" tilecount="12544" columns="112">
+ <image source="Downloads/sprites tiled/Mossy - TileSet.png" width="3584" height="3584"/>
+</tileset>
