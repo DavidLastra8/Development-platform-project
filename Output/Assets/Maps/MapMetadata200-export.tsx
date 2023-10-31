b3a6022b57520f2a30b84e6e432278bf1c241677
DavidLastra8 <david.lastra@estudiantat.upc.edu>
2023-10-31 12:10:58 +0100

    Map 1.0

---
+++ Output/Assets/Maps/MapMetadata200-export.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="MapMetadata200-export" tilewidth="64" tileheight="64" tilecount="2" columns="2">
+ <image source="MapMetadata200-export.png" width="134" height="68"/>
+</tileset>
---
+++ Output/Assets/Maps/Mossy - TileSet.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="Mossy - TileSet" tilewidth="64" tileheight="64" tilecount="3136" columns="56">
+ <image source="../../../../../../Downloads/Mossy Assets/Mossy Tileset/Mossy - TileSet.png" width="3584" height="3584"/>
+</tileset>
