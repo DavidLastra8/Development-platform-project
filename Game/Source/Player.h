fa08c46189b0bfbf1914e9ca05c860a509bc71a6
Yeeway69 <yeyiwei1@gmail.com>
2023-12-17 21:34:40 +0100

    PlayerAnimation finished

--- Game/Source/Player.h
+++ Game/Source/Player.h
@@ -79,6 +79,8 @@ public:
 	Animation idleLeftAnim;
 	Animation walkLeftAnim;
 	Animation walkRightAnim;
+	Animation fallingLeftAnim;
+	Animation fallingRightAnim;
 
 	Direction lastDirection = IDLE;
 
